5d74c27fcf694fdbd6c62ea6ca96bac633834631
andiizhu <azhu0317@gmail.com>
2024-04-19 01:53:59 -0400

    Q2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,36 +1,36 @@
-const helloWorldRegex = (str) => /Hello world/im.test(str)
+const helloWorldRegex = (str) => /Hello world/im.test(str);
 // checking for the string hello world is matched within any of the given strings
 // i, m = the letter i in regex lets us dismiss the case sensitivity, m allows us to check for a pattern from start to end in string.
 
-const hasAVowel = (str) => { };
+const hasAVowel = (str) => /[aeiou]/gi.test(str);
 
-const hasCatsOrDogs = (str) => { };
+const hasCatsOrDogs = (str) => {};
 
-const hasVowelStart = (str) => { };
+const hasVowelStart = (str) => {};
 
-const hasPunctuationEnd = (str) => { };
+const hasPunctuationEnd = (str) => {};
 
-const hasNothingOrDigits = (str) => { };
+const hasNothingOrDigits = (str) => {};
 
-const hasNoFlippers = (str) => { };
+const hasNoFlippers = (str) => {};
 
-const isValidEmail = (str) => { };
+const isValidEmail = (str) => {};
 
-const isValidPhoneNumber = (str) => { };
+const isValidPhoneNumber = (str) => {};
 
-const matchAllNumbers = (str) => { };
+const matchAllNumbers = (str) => {};
 
-const matchAllNumbersAsNumbers = (str) => { };
+const matchAllNumbersAsNumbers = (str) => {};
 
-const matchAllWords = (str) => { };
+const matchAllWords = (str) => {};
 
-const replaceAllNumbers = (str) => { };
+const replaceAllNumbers = (str) => {};
 
-const fixFileName = (str) => { };
+const fixFileName = (str) => {};
 
-const nameRedacter = (str) => { };
+const nameRedacter = (str) => {};
 
-const camelToSnakeCase = (str) => { };
+const camelToSnakeCase = (str) => {};
 
 module.exports = {
   helloWorldRegex,
