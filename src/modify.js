15bdd27ddc053e64dab08bc2bb0ec5fc1a65e1a4
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-04-19 03:05:28 -0400

    debug corrected

--- src/modify.js
+++ src/modify.js
@@ -1,11 +1,14 @@
+const swapAllCases = (str) => {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const isUppercaseLetter = char === char.toUpperCase();
+    result += isUppercaseLetter ? char.toLowerCase() : char.toUpperCase();
+  }
 
-const isValidCompanyUsername = (username, firstName, lastName) => {
-  const regex = new RegExp(`^(sales|tech)\\d\\w-${firstName[0]}${lastName}\\d?$`);
-  // console.log(regex) // to help you debug, look at the final regex
-  return regex.test(username);
+  return result;
 };
 
-m
 module.exports = {
-  isValidCompanyUsername,
+  swapAllCases,
 };
