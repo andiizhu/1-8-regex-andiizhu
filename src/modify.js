8c5d011289bdb9b95aae5a60c462af43737732b3
andiizhu <azhu0317@gmail.com>
2024-04-19 03:09:54 -0400

    Finished Assignment

--- src/modify.js
+++ src/modify.js
@@ -1,14 +1,20 @@
 const swapAllCases = (str) => {
-  let result = '';
+  let result = "";
   for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    const isUppercaseLetter = char === char.toUpperCase();
-    result += isUppercaseLetter ? char.toLowerCase() : char.toUpperCase();
+    if (/[A-Z]/.test(str[i])) {
+      result += str[i].toLowerCase();
+    } else result += str[i].toUpperCase();
   }
-
   return result;
 };
 
+// const swapAllCases = (str) => {
+//   const changeCase = (match) => { //callback function
+//     if (/[a-z]/.stte(match)) return match.toUpperCase(); if regex /a-z/.test(<argument>) is true then uppercase the arguement.
+//     if (/[A-Z]/.test(match)) return match.toLowerCase(); if regex /A-Z/.test(<argument) is true then lowercase the argument.
+//   };
+//   return (str.replaceAll(/[a-zA-Z]/g, changeCase));// .replace replaces the first thing only. .replaceAll replaces everything.
+// };
 module.exports = {
   swapAllCases,
 };
